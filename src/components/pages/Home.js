7410bafa0d68d50a7eaaed73658ce3498392e67f
AlbertJovner3 <81398199+AlbertJovner3@users.noreply.github.com>
2021-08-24 18:41:54 +1200

    Update Home.js

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,10 +1,10 @@
 import React, {useState} from 'react'
-import Sidebar from '../components/Sidebar.js'
-import Navbar from '../components/Navbar.js';
+import Sidebar from './components/Sidebar.js'
+import Navbar from './components/Navbar.js';
 import '../../App.css';
 import Cards from '../Cards';
-import HeroSection from '../HeroSection';
-import Footer from '../Footer';
+import HeroSection from './HeroSection';
+import Footer from './Footer';
 
 const Home = () => {
     const [isOpen, setIsOpen] = useState(false)

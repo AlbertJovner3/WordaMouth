4d2d338a375eb8287337ca00db186271f88378c4
AlbertJovner3 <81398199+AlbertJovner3@users.noreply.github.com>
2021-08-24 18:35:14 +1200

    Update Home.js

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
-import Sidebar from '../Components/Sidebar/Sidebar.js'
-import Navbar from '../Components/Navbar/Navbar.js';
+import Sidebar from '../Components/Sidebar.js'
+import Navbar from '../Components/Navbar.js';
 import '../../App.css';
 import Cards from '../Cards';
 import HeroSection from '../HeroSection';

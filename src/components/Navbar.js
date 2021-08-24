ffca9084b728ec065ad4aa2a0a5350066f26727f
AlbertJovner3 <81398199+AlbertJovner3@users.noreply.github.com>
2021-08-24 16:17:11 +1200

    Updating the the website design by moving and adding new and existing components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,70 +1,48 @@
-import React, { useState, useEffect } from 'react';
-import { Buttontwo } from './Buttontwo';
-import { Link } from 'react-router-dom';
-import './Navbar.css';
+import React from 'react';
+import {FaBars} from 'react-icons/fa';
+import { 
+    Nav, 
+    NavbarBox, 
+    NavLogo, 
+    MobileIcon, 
+    NavMenu, 
+    NavItem, 
+    NavLinks
+    // NavBtn,
+    // NavBtnLink
+ } from './NavbarElement';
 
-function Navbar() {
-  const [click, setClick] = useState(false);
-  const [button, setButton] = useState(true);
 
-  const handleClick = () => setClick(!click);
-  const closeMobileMenu = () => setClick(false);
-
-  const showButton = () => {
-    if (window.innerWidth <= 960) {
-      setButton(false);
-    } else {
-      setButton(true);
-    }
-  };
-
-  useEffect(() => {
-    showButton();
-  }, []);
-
-  window.addEventListener('resize', showButton);
-
-  return (
+const Navbar = ({ toggle }) => {
+    return (
     <>
-      <nav className='navbar'>
-        <div className='navbar-container'>
-          <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
-          Word 'a Mouth' <img src="images/logo.png" alt="logo" width="45" height="45"></img>
-          </Link>
-          <div className='menu-icon' onClick={handleClick}>
-            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
-          </div>
-          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-            <li className='nav-item'>
-              <Link to='/' className='nav-links' onClick={closeMobileMenu}>
-                Home
-              </Link>
-            </li>
-            <li className='nav-item'>
-              <Link
-                to='/places'
-                className='nav-links'
-                onClick={closeMobileMenu}
-              >
-                Restaurants
-              </Link>
-            </li>
+        <Nav>
+            <NavbarBox>
+                <NavLogo to='/'>WM</NavLogo>
+                <MobileIcon onClick={toggle}>
+                    <FaBars />
+                </MobileIcon>
+                <NavMenu>
+                    <NavItem>
+                        <NavLinks to= "town">Town</NavLinks>
+                    </NavItem>
+                    <NavItem>
+                        <NavLinks to= "menu">Menu</NavLinks>
+                    </NavItem>
+                    <NavItem>
+                        <NavLinks to= "about">About Us</NavLinks>
+                    </NavItem>
+                </NavMenu>
 
-            <li>
-              <Link
-                to='/signin'
-                className='nav-links-mobile'
-                onClick={closeMobileMenu}
-              >
-                Sign in
-              </Link>
-            </li>
-          </ul>
-          {button && <Buttontwo buttonStyle='btn2--outline'>Sign in</Buttontwo>}
-        </div>
-      </nav>
+                {/* Will use it for the footer 
+                <NavBtn>
+                    <NavBtnLink to='/signin'>Sign In</NavBtnLink>
+                </NavBtn> */}
+                
+            </NavbarBox>
+        </Nav>
     </>
-  );
-}
+    );
+};
 
 export default Navbar;

ba21d5df903d66b08c806671ca26a676bd82be57
AlbertJovner3 <81398199+AlbertJovner3@users.noreply.github.com>
2021-08-25 16:41:28 +1200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Navbar from './components/Navbar';
 import './App.css';
 import {BrowserRouter as Router} from 'react-router-dom';
 import Home from './components/pages/Home';

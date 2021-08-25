f352d026aa393d65d26b11a0c79bf09cf42e579b
AlbertJovner3 <81398199+AlbertJovner3@users.noreply.github.com>
2021-08-25 16:40:07 +1200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import React from 'react';
 import './App.css';
 import {BrowserRouter as Router} from 'react-router-dom';
 import Home from './components/pages/Home';
-
+import Places from './components/pages/Places';
+import Signin from './components/pages/Signin';
 
 function App() {
     return (
         <Router>
-            <Home />
-        </Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/places' exact component={Places} />
+          <Route path='/signin' component={Signin} />
+        </Switch>
+      </Router>
     );
 };
 

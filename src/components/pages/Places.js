cd9575e58661167e69d431a18d0afeb6be782816
AlbertJovner3 <81398199+AlbertJovner3@users.noreply.github.com>
2021-08-31 11:59:44 +1200

    Update Places.js

--- src/components/pages/Places.js
+++ src/components/pages/Places.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import Sidebar from '../Sidebar.js'
 import Navbar from '../Navbar.js';
 import '../../App.css';
 import Footer from '../Footer';
 
-function Places() {
+const Places= () => {
+  const [isOpen, setIsOpen] = useState(false)
+    
+    const toggle = () => {
+      setIsOpen(!isOpen)
+    }
+    
     return (
   <div className="player-wrapper">
+    <Sidebar isOpen={isOpen} toggle={toggle} />
         <Navbar toggle={toggle} />
     <h1>Restraunt List here:</h1>
     <Footer />

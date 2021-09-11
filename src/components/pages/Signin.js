ad2a27d984c915521778eb7bd997c01aa3c21e6a
AlbertJovner3 <81398199+AlbertJovner3@users.noreply.github.com>
2021-09-11 19:57:27 +1200

    Added jongbo elemnts v1

---
+++ src/components/pages/Signin.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Signinform from '../components/Signin/Signinform';
+
+const SignIn = () => {
+    return (
+        <>
+            <Signinform />
+        </>
+    );
+};
+
+export default SignIn;

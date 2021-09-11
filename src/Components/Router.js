09c19d32270d562d02b23a5b44c900b634b2b894
AlbertJovner3 <81398199+AlbertJovner3@users.noreply.github.com>
2021-09-11 15:08:25 +1200

    Update Router.js

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
-import Home from './Components/routes/home';
-import Places from './Components/routes/Places';
-import Place from './Components/routes/Place';
-import Signin from './Components/routes/Signin';
+import Home from './routes/home';
+import Places from './routes/Places';
+import Place from './routes/Place';
+import Signin from './routes/Signin';
 
 
 const AppRouter = ({ isLoggedIn }) => {

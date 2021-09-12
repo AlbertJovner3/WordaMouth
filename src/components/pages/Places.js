6610b56419b310c0573582af7251780cf0d84bf8
AlbertJovner3 <81398199+AlbertJovner3@users.noreply.github.com>
2021-09-12 17:54:03 +1200

    v0.02 Update

--- src/components/pages/Places.js
+++ src/components/pages/Places.js
@@ -1,38 +1,10 @@
 import React, {useState, useEffect} from 'react'
 import '../../App.css';
-import {Link} from 'react-router-dom';
-function Places() {
-    useEffect(() =>{
-fetchItems();
-    },[]);
-    const [items, setItems] = useState([], );
-    const fetchItems = async () =>{
-        const url = `https://scpalbert-eac6.restdb.io/rest/scp?q={}&sort=id`;
-        const data = await fetch(url,{method: 'GET',
-    headers:{
-        'cache-control': 'no-cache',
-        'x-apikey': '60c16401f2a01c34b8ed36fb'
-    }});
-    const scp = await data.json();
-    console.log(scp);
-    setItems(scp);
-}
-    return (
-        <div>
-        <div className='hero-container'>
-      <video alt="Background" src='../../videos/backgroundscpvid.mp4' autoPlay loop muted />
-      <h1>Local Cuisine</h1>
-      <p>Whats in your area</p>
-    </div>
-    <div className='content'>
-        <h2>?</h2>
-        {items.map( item =>(
-         <Link to={`/places/${item._id}`}><h4 key={item._id}>{item.name}</h4></Link>
-        ))}
-    </div>
-    </div>
-    )
-}
-   
+//import {Link} from 'react-router-dom';
 
-export default Places
+function Place() {
+<div>
+<h1>Places</h1>
+</div>
+}
+export default Place;
